81e2ace78af2c34cef5c08b8f6fecbf3e665b491
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-10-11 03:21:57 -0400

    added variable speed

--- src/components/BubbleSortVisualizer.js
+++ src/components/BubbleSortVisualizer.js
@@ -1,85 +1,107 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function BubbleSortVisualizer() {
-    const generateRandomData = (length, min, max) => {
-        let numbers = new Set();
-        while (numbers.size < length) {
-            numbers.add(Math.floor(Math.random() * (max - min + 1) + min));
+    const shuffleArray = arr => {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
         }
-        return [...numbers];
-    }
+    };
 
-    const [numItems, setNumItems] = useState(11);
-    const [data, setData] = useState(generateRandomData(numItems, 1, 2 * numItems));
-    const [activeIndices, setActiveIndices] = useState([]);
+    const generateData = length => {
+        const numbers = Array.from({ length }, (_, i) => i + 1);
+        shuffleArray(numbers);
+        return numbers;
+    };
+
+    const BASE_SPEED = 200; // Base delay in milliseconds
+    const [state, setState] = useState({
+        numItems: 10,
+        data: generateData(10),
+        activeIndices: [],
+        speedMultiplier: 1  // Default to 1x speed
+    });
+
+    const stopSorting = useRef(false); // Reference to control the sorting
 
     useEffect(() => {
-        setData(generateRandomData(numItems, 1, 2 * numItems));
-    }, [numItems]);
+        setState(prevState => ({ ...prevState, data: generateData(state.numItems) }));
+    }, [state.numItems]);
 
     const bubbleSort = async () => {
-        let arr = [...data];
-        let n = arr.length;
-        
-        for (let i = 0; i < n-1; i++) {
-            for (let j = 0; j < n-i-1; j++) {
-                setActiveIndices([j, j+1]); // Highlight the elements being compared
-                await new Promise(resolve => setTimeout(resolve, 0.1)); // Introduce a delay
-    
-                if (arr[j] > arr[j+1]) {
-                    // swap arr[j] and arr[j+1]
-                    let temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-    
-                    setData([...arr]);
-                    await new Promise(resolve => setTimeout(resolve, 0.1)); // Introduce a delay for swap visualization
+        let arr = [...state.data];
+        const delay = BASE_SPEED / state.speedMultiplier;
+        stopSorting.current = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            for (let j = 0; j < arr.length - i - 1; j++) {
+                if (stopSorting.current) return;
+
+                setState(prevState => ({ ...prevState, activeIndices: [j, j + 1] }));
+                await new Promise(resolve => setTimeout(resolve, delay));
+
+                if (arr[j] > arr[j + 1]) {
+                    [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                    setState(prevState => ({ ...prevState, data: arr }));
+                    await new Promise(resolve => setTimeout(resolve, delay));
                 }
             }
         }
-        setActiveIndices([]); // Clear the highlighted elements after sorting
-    }
+        setState(prevState => ({ ...prevState, activeIndices: [] }));
+    };
 
-    const randomizeData = () => {
-        setData(generateRandomData(numItems, 1, 2 * numItems));
-    }
+    const handleRandomize = () => {
+        stopSorting.current = true;  // Signal to stop the sorting
+        setState(prevState => ({ ...prevState, data: generateData(state.numItems), activeIndices: [] }));
+    };    
 
-    const handleNumItemsChange = (event) => {
-        const value = parseInt(event.target.value, 10);
-        if (!isNaN(value) && value > 0) {
-            setNumItems(value);
-        }
-    }
-
-    const maxNumber = Math.max(...data);
-    const barWidthPercentage = 100 / numItems;
+    const maxNumber = Math.max(...state.data);
+    const barWidth = 100 / state.numItems;
 
     return (
         <div className='flex flex-col justify-center items-center h-screen w-full space-y-4'>
             <h1 className='text-4xl mb-10'>Bubble Sort</h1>
-            <div className="flex justify-center items-end max-w-3xl" style={{height: '400px', width: '90%', gap: '2px'}}>
-                {data.map((value, idx) => (
+            <div className="flex justify-center items-end max-w-3xl" style={{ height: '400px', width: '90%', gap: '2px' }}>
+                {state.data.map((value, idx) => (
                     <div 
-                        key={idx} 
-                        style={{
-                            height: `${(value / maxNumber) * 100}%`,
-                            width: `${barWidthPercentage}%`
-                        }}
-                        className={`${activeIndices.includes(idx) ? 'bg-customPink' : 'bg-customLightBlue'}`}
-                    ></div>
+                        key={idx}
+                        style={{ height: `${(value / maxNumber) * 100}%`, width: `${barWidth}%` }}
+                        className={`${state.activeIndices.includes(idx) ? 'bg-customPink' : 'bg-customLightBlue'}`}
+                    />
                 ))}
             </div>
-            <div className='flex'>
-                <button className='px-4 py-1 text-2xl bg-customLightBlue rounded-lg mr-4' onClick={bubbleSort}>Sort</button>
-                <button className='px-4 py-1 text-2xl bg-customLightBlue rounded-lg' onClick={randomizeData}>Randomize</button>
-                <input
-                    type="number"
-                    min="1"
-                    value={numItems}
-                    onChange={handleNumItemsChange}
-                    className="mx-4 px-2 py-1 border rounded w-20"
-                    placeholder="Number of items"
-                />
+            <div className='flex gap-4'>
+                <button className='px-4 py-1 text-2xl bg-customLightBlue rounded-lg' onClick={bubbleSort}>Sort</button>
+                <button className='px-4 py-1 text-2xl bg-customLightBlue rounded-lg' onClick={handleRandomize}>Randomize</button>
+                <div className='flex gap-2 items-center'>
+                <label>n =</label>
+                    <input
+                        type="number"
+                        min="1"
+                        value={state.numItems}
+                        onChange={e => {
+                            const value = parseInt(e.target.value, 10);
+                            value > 0 && setState(prevState => ({ ...prevState, numItems: value }));
+                        }}
+                        className="px-2 py-1 border rounded w-20"
+                        placeholder="Number of items"
+                    />
+                </div>
+                
+                <div className='flex gap-2 items-center'>
+                    <label>Speed:</label>
+                    <select 
+                        value={state.speedMultiplier}
+                        onChange={e => setState(prevState => ({ ...prevState, speedMultiplier: parseFloat(e.target.value) }))}
+                        className="border rounded">
+                        <option value={0.25}>0.25x</option>
+                        <option value={0.5}>0.5x</option>
+                        <option value={1}>1x</option>
+                        <option value={2}>2x</option>
+                        <option value={4}>4x</option>
+                        <option value={8}>8x</option>
+                        <option value={16}>16x</option>
+                    </select>
+                </div>
             </div>
         </div>
     );
