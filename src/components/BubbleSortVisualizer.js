812ef8da311fed09e709713778292f3cdf207aae
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-10-13 18:03:22 -0400

    Finished BubbleSort

--- src/components/BubbleSortVisualizer.js
+++ src/components/BubbleSortVisualizer.js
@@ -14,12 +14,14 @@ function BubbleSortVisualizer() {
         return numbers;
     };
 
-    const BASE_SPEED = 100; // Base delay in milliseconds
+    const computeBaseSpeed = () => 1000 / state.numItems;
+
     const [state, setState] = useState({
         numItems: 10,
         data: generateData(10),
         activeIndices: [],
-        speedMultiplier: 1  // Default to 1x speed
+        completedIndices: [],
+        speedMultiplier: 1
     });
 
     const stopSorting = useRef(false); // Reference to control the sorting
@@ -28,9 +30,18 @@ function BubbleSortVisualizer() {
         setState(prevState => ({ ...prevState, data: generateData(state.numItems) }));
     }, [state.numItems]);
 
+    const highlightAllBarsSequentially = () => {
+        const delay = 20;
+        for (let i = 0; i < state.data.length; i++) {
+            setTimeout(() => {
+                setState(prevState => ({ ...prevState, completedIndices: [...prevState.completedIndices, i] }));
+            }, i * delay);
+        }
+    };   
+
     const bubbleSort = async () => {
         let arr = [...state.data];
-        const delay = BASE_SPEED / state.speedMultiplier;
+        const delay = computeBaseSpeed() / state.speedMultiplier;
         stopSorting.current = false;
         for (let i = 0; i < arr.length - 1; i++) {
             for (let j = 0; j < arr.length - i - 1; j++) {
@@ -46,38 +57,44 @@ function BubbleSortVisualizer() {
                 }
             }
         }
-        setState(prevState => ({ ...prevState, activeIndices: [] }));
+        highlightAllBarsSequentially();
     };
 
     const handleRandomize = () => {
         stopSorting.current = true;  // Signal to stop the sorting
-        setState(prevState => ({ ...prevState, data: generateData(state.numItems), activeIndices: [] }));
-    };    
+        setState(prevState => ({ ...prevState, data: generateData(state.numItems), activeIndices: [], completedIndices: [] }));
+    };
+
+    useEffect(() => {
+        stopSorting.current = true;  // Signal to stop the sorting
+        setState(prevState => ({ ...prevState, data: generateData(state.numItems), activeIndices: [], completedIndices: [] }));
+    }, [state.numItems]);
 
     const maxNumber = Math.max(...state.data);
-    const isSmallScreen = window.innerWidth < 640; // Check if screen width is less than 640px
-    const barWidth = isSmallScreen ? (100 / state.numItems) / 2 : 100 / state.numItems;
-
+    const isMediumScreen = window.innerWidth < 768;
+    const barWidth = 100 / state.numItems;
 
     return (
-        <div className='flex flex-col justify-center items-center h-screen w-full space-y-4'>
-            <h1 className='text-4xl mb-10'>Bubble Sort</h1>
-            <div className="flex justify-center items-end max-w-3xl" style={{ height: '400px', width: '90%', gap: '2px' }}>
+        <div className='flex flex-col justify-center items-center h-screen w-full space-y-4 pt-12'>
+            <h1 className='text-4xl my-10'>Bubble Sort</h1>
+            <div className="flex justify-center items-end max-w-4xl" style={{ height: '400px', width: '90%', gap: '2px' }}>
                 {state.data.map((value, idx) => (
                     <div 
                         key={idx}
                         style={{ height: `${(value / maxNumber) * 100}%`, width: `${barWidth}%` }}
-                        className={`${state.activeIndices.includes(idx) ? 'bg-customPink' : 'bg-customLightBlue'}`}
+                        className={`
+                            ${state.activeIndices.includes(idx) ? 'bg-customPink' : ''}
+                            ${state.completedIndices.includes(idx) ? 'bg-customPurple' : ''}
+                            ${!state.activeIndices.includes(idx) && !state.completedIndices.includes(idx) ? 'bg-customLightBlue' : ''}
+                        `}
                     />
                 ))}
             </div>
-            <div className='flex flex-col-reverse sm:flex-row gap-4 w-full max-w-xl'>
-
+            <div className='flex flex-col-reverse sm:flex-row gap-4 w-full max-w-xl pt-10'>
                 <div className='flex justify-center gap-4 w-full'>
                     <button className='px-4 py-1 text-2xl bg-customLightBlue rounded-lg' onClick={bubbleSort}>Sort</button>
                     <button className='px-4 py-1 text-2xl bg-customLightBlue rounded-lg' onClick={handleRandomize}>Randomize</button>
                 </div>
-
                 <div className='flex justify-center gap-4 w-full'>
                     <div className='flex gap-2 items-center'>
                         <label>n =</label>
@@ -87,15 +104,23 @@ function BubbleSortVisualizer() {
                             value={state.numItems}
                             onChange={e => {
                                 const value = parseInt(e.target.value, 10);
-                                value > 0 && setState(prevState => ({ ...prevState, numItems: value }));
-                            }}
+                            
+                                if (value < 5) {
+                                    setState(prevState => ({ ...prevState, numItems: 5 }));
+                                } else if (isMediumScreen && value > 50) {
+                                    setState(prevState => ({ ...prevState, numItems: 50 }));
+                                } else if (!isMediumScreen && value > 100) {
+                                    setState(prevState => ({ ...prevState, numItems: 100 }));
+                                } else {
+                                    setState(prevState => ({ ...prevState, numItems: value }));
+                                }
+                            }}                            
                             className="px-2 py-1 border rounded w-20"
                             placeholder="Number of items"
                         />
                     </div>
-                    
-                    <div className='flex gap-2 items-center'>
-                        <label>Speed:</label>
+                    <div className='flex gap-2'>
+                        <label className="self-center">Speed:</label>
                         <select 
                             value={state.speedMultiplier}
                             onChange={e => setState(prevState => ({ ...prevState, speedMultiplier: parseFloat(e.target.value) }))}
@@ -110,9 +135,6 @@ function BubbleSortVisualizer() {
                         </select>
                     </div>
                 </div>
-                
-                
-
             </div>
         </div>
     );
