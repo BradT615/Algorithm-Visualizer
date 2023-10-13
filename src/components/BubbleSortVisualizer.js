5499fc6aac9c716a70e1c722e21fc4ca690b742c
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-10-12 22:15:59 -0400

    mobile button layout

--- src/components/BubbleSortVisualizer.js
+++ src/components/BubbleSortVisualizer.js
@@ -14,7 +14,7 @@ function BubbleSortVisualizer() {
         return numbers;
     };
 
-    const BASE_SPEED = 200; // Base delay in milliseconds
+    const BASE_SPEED = 100; // Base delay in milliseconds
     const [state, setState] = useState({
         numItems: 10,
         data: generateData(10),
@@ -55,7 +55,9 @@ function BubbleSortVisualizer() {
     };    
 
     const maxNumber = Math.max(...state.data);
-    const barWidth = 100 / state.numItems;
+    const isSmallScreen = window.innerWidth < 640; // Check if screen width is less than 640px
+    const barWidth = isSmallScreen ? (100 / state.numItems) / 2 : 100 / state.numItems;
+
 
     return (
         <div className='flex flex-col justify-center items-center h-screen w-full space-y-4'>
@@ -69,39 +71,48 @@ function BubbleSortVisualizer() {
                     />
                 ))}
             </div>
-            <div className='flex gap-4'>
-                <button className='px-4 py-1 text-2xl bg-customLightBlue rounded-lg' onClick={bubbleSort}>Sort</button>
-                <button className='px-4 py-1 text-2xl bg-customLightBlue rounded-lg' onClick={handleRandomize}>Randomize</button>
-                <div className='flex gap-2 items-center'>
-                <label>n =</label>
-                    <input
-                        type="number"
-                        min="1"
-                        value={state.numItems}
-                        onChange={e => {
-                            const value = parseInt(e.target.value, 10);
-                            value > 0 && setState(prevState => ({ ...prevState, numItems: value }));
-                        }}
-                        className="px-2 py-1 border rounded w-20"
-                        placeholder="Number of items"
-                    />
+            <div className='flex flex-col-reverse sm:flex-row gap-4 w-full max-w-xl'>
+
+                <div className='flex justify-center gap-4 w-full'>
+                    <button className='px-4 py-1 text-2xl bg-customLightBlue rounded-lg' onClick={bubbleSort}>Sort</button>
+                    <button className='px-4 py-1 text-2xl bg-customLightBlue rounded-lg' onClick={handleRandomize}>Randomize</button>
+                </div>
+
+                <div className='flex justify-center gap-4 w-full'>
+                    <div className='flex gap-2 items-center'>
+                        <label>n =</label>
+                        <input
+                            type="number"
+                            min="1"
+                            value={state.numItems}
+                            onChange={e => {
+                                const value = parseInt(e.target.value, 10);
+                                value > 0 && setState(prevState => ({ ...prevState, numItems: value }));
+                            }}
+                            className="px-2 py-1 border rounded w-20"
+                            placeholder="Number of items"
+                        />
+                    </div>
+                    
+                    <div className='flex gap-2 items-center'>
+                        <label>Speed:</label>
+                        <select 
+                            value={state.speedMultiplier}
+                            onChange={e => setState(prevState => ({ ...prevState, speedMultiplier: parseFloat(e.target.value) }))}
+                            className="border rounded">
+                            <option value={0.25}>0.25x</option>
+                            <option value={0.5}>0.5x</option>
+                            <option value={1}>1x</option>
+                            <option value={2}>2x</option>
+                            <option value={4}>4x</option>
+                            <option value={8}>8x</option>
+                            <option value={16}>16x</option>
+                        </select>
+                    </div>
                 </div>
                 
-                <div className='flex gap-2 items-center'>
-                    <label>Speed:</label>
-                    <select 
-                        value={state.speedMultiplier}
-                        onChange={e => setState(prevState => ({ ...prevState, speedMultiplier: parseFloat(e.target.value) }))}
-                        className="border rounded">
-                        <option value={0.25}>0.25x</option>
-                        <option value={0.5}>0.5x</option>
-                        <option value={1}>1x</option>
-                        <option value={2}>2x</option>
-                        <option value={4}>4x</option>
-                        <option value={8}>8x</option>
-                        <option value={16}>16x</option>
-                    </select>
-                </div>
+                
+
             </div>
         </div>
     );
