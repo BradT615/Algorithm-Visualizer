69f183a9447f687a16c80f8d8664f52b3586942d
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-11-25 01:04:18 -0500

    fixed Navbar shadow

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo-removebg.png';
 
 function Navbar() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            // Check if page is scrolled more than 10 pixels
+            if (window.scrollY > 10) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="fixed w-full bg-customBlue p-3 z-10 shadow-xl">
+        <nav className={`fixed w-full bg-customBlue p-3 z-10 transition duration-500 ${isScrolled ? 'shadow-xl' : ''}`}>
             <Link to="/">
                 <div className='flex justify-center items-center text-2xl gap-1'>
                     <h1>Algorithm</h1>
