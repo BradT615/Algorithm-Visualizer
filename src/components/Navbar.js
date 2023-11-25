929525d2b86050db19f0b1193a85443b12b130a1
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-11-25 01:18:57 -0500

    scale navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,15 +24,15 @@ function Navbar() {
     return (
         <nav className={`fixed w-full bg-customBlue p-3 z-10 transition duration-500 ${isScrolled ? 'shadow-xl' : ''}`}>
             <Link to="/">
-                <div className='flex justify-center items-center text-2xl gap-1'>
+                <div className='flex justify-center items-center text-md sm:text-2xl gap-1'>
                     <h1>Algorithm</h1>
-                    <img src={logo} className='w-10' alt="Logo" />
+                    <img src={logo} className='w-6 sm:w-10' alt="Logo" />
                     <h1>Visualizer</h1>
                 </div>
             </Link>
-            <div className="absolute top-2 right-2 p-1 hidden sm:block transform hover:scale-105 transition-transform hover:text-customOffWhite">
+            <div className="absolute top-1 right-2 p-1 text-4xl sm:text-5xl hover:text-customOffWhite">
                 <a href="https://github.com/BradT615/Algorithm-Visualizer" target="_blank" rel="noopener noreferrer">
-                    <FaGithub size={40} />
+                    <FaGithub/>
                 </a>
             </div>
         </nav>
