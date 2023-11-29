124aab5f4e12419edf4d0916f5d4c053075fe8fa
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-11-29 02:30:01 -0500

    added custom images for sorts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo-removebg.png';
 
 function Home() {
     const algorithmNames = [
@@ -8,7 +7,18 @@ function Home() {
         "InsertionSort", "ShellSort", "GnomeSort", "PatienceSort",
         "SelectionSort", "HeapSort","MergeSort", "TimSort",
         "CycleSort", "BucketSort", "RadixSort", "BogoSort"
-    ];    
+    ];
+
+    const getImagePath = (algorithmName) => {
+        try {
+            // Dynamically import the image based on the algorithm name
+            return require(`../assets/sortImages/${algorithmName}.png`);
+        } catch (error) {
+            console.error('Image not found for algorithm:', algorithmName);
+            // Fallback image or handle error as required
+            return null;
+        }
+    };
 
     return (
         <div className="h-full max-w-7xl m-auto flex flex-col justify-center items-center">
@@ -16,7 +26,7 @@ function Home() {
                 {algorithmNames.map((name, i) => (
                     <Link key={i} to={`/${name}`}>
                         <div className="bg-customBlue text-customOffWhite text-2xl p-4 flex flex-col items-center rounded-3xl w-full transform hover:scale-105 transition-transform">
-                            <img src={logo} alt="Algorithm Logo" className=" p-6" />
+                            <img src={getImagePath(name)} alt={`${name} Algorithm`} className="rounded-full bg-transparent p-6" />
                             {name}
                         </div>
                     </Link>
@@ -24,7 +34,6 @@ function Home() {
             </div>
         </div>
     );
-    
 }
 
 export default Home;
