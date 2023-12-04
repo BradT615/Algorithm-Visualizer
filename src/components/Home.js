6195bd23f81fc1196e4aed611b099be665b27716
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-12-04 01:28:29 -0500

    revert custom images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo-removebg.png';
 
 function Home() {
     const algorithmNames = [
@@ -7,47 +8,23 @@ function Home() {
         "InsertionSort", "ShellSort", "GnomeSort", "PatienceSort",
         "SelectionSort", "HeapSort","MergeSort", "TimSort",
         "CycleSort", "BucketSort", "RadixSort", "BogoSort"
-    ];
-
-    const getImagePath = (algorithmName) => {
-        try {
-            // Dynamically import the image based on the algorithm name
-            return require(`../assets/sortImages/${algorithmName}.png`);
-        } catch (error) {
-            console.error('Image not found for algorithm:', algorithmName);
-            // Fallback image or handle error as required
-            return null;
-        }
-    };
+    ];    
 
     return (
         <div className="h-full max-w-7xl m-auto flex flex-col justify-center items-center">
-            <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-6 mt-24 p-8 sm:p-24 md:p-10">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-24 p-10">
                 {algorithmNames.map((name, i) => (
                     <Link key={i} to={`/${name}`}>
-                        <div className="bg-customCyberBlue text-customOffWhite p-4 flex flex-col items-center rounded-lg lg:rounded-2xl w-full glow-effect transform hover:scale-105 transition-transform">
-                            <img 
-                                src={getImagePath(name)} 
-                                alt={`${name} Algorithm`} 
-                                onError={(e) => {
-                                    if (e.currentTarget.src !== '../assets/logo-removebg.png') { // Replace with your actual fallback image path
-                                        e.currentTarget.src = '../assets/logo-removebg.png'; // Fallback image
-                                        e.currentTarget.onerror = null; // Prevents infinite loop if fallback image also fails
-                                    }
-                                }}
-                                key={name} // Adding key to help React identify the images uniquely
-                            />
-                            <div className="text-center text-xl sm:text-xl lg:text-2xl xl:text-3xl"> {/* Responsive text sizing */}
-                                {name}
-                            </div>
+                        <div className="bg-customBlue text-customOffWhite text-2xl p-4 flex flex-col items-center rounded-3xl w-full glow-effect transform hover:scale-105 transition-transform">
+                            <img src={logo} alt="Algorithm Logo" className=" p-6" />
+                            {name}
                         </div>
                     </Link>
                 ))}
             </div>
         </div>
     );
-    
-     
+
 }
 
 export default Home;
