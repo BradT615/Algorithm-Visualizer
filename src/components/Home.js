11048b9b65fc5d3ede7243780ac918f4b1cc3c2e
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-12-10 00:27:57 -0500

    updated layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,12 +12,12 @@ function Home() {
 
     return (
         <div className="h-full max-w-7xl m-auto flex flex-col justify-center items-center">
-            <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-6 mt-24 p-10 sm:p-12 md:p-10">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-2 sm:gap-6 mt-14 sm:mt-20 md:mt-24 p-4 md:p-10">
                 {algorithmNames.map((name, i) => (
                     <Link key={i} to={`/${name}`}>
-                        <div className="bg-customBlue text-customOffWhite text-2xl p-4 flex flex-col items-center rounded-3xl w-full glow-effect transform hover:scale-105 transition-transform">
-                            <img src={logo} alt="Algorithm Logo" className=" p-6" />
-                            <div className="text-center text-xl sm:text-xl lg:text-2xl xl:text-3xl">
+                        <div className="bg-customBlue text-customOffWhite text-2xl max-w-[200px] p-4 flex flex-col items-center rounded-lg sm:rounded-xl md:rounded-2xl w-full glow-effect transform hover:scale-105 transition-transform">
+                            <img src={logo} alt="Algorithm Logo" className=" p-1 md:p-6" />
+                            <div className="text-center text-sm sm:text-xl lg:text-2xl">
                                 {name}
                             </div>
                         </div>
