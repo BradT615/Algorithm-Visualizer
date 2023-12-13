942878bef62328b27c82d66e61c10af3f4f3297d
BradT615 <125227539+BradT615@users.noreply.github.com>
2023-12-13 03:29:28 -0500

    prevent highlight

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,8 +15,8 @@ function Home() {
             <div className="grid grid-cols-2 md:grid-cols-4 gap-2 sm:gap-6 mt-14 sm:mt-20 md:mt-24 p-4 md:p-10">
                 {algorithmNames.map((name, i) => (
                     <Link key={i} to={`/${name}`}>
-                        <div className="bg-customBlue text-customOffWhite text-2xl max-w-[200px] p-4 flex flex-col items-center rounded-lg sm:rounded-xl md:rounded-2xl w-full glow-effect transform hover:scale-105 transition-transform">
-                            <img src={logo} alt="Algorithm Logo" className=" p-1 md:p-6" />
+                        <div className="bg-customBlue text-customOffWhite text-2xl max-w-[200px] p-4 flex flex-col items-center rounded-lg sm:rounded-xl md:rounded-2xl w-full glow-effect transform hover:scale-105 transition-transform prevent-select">
+                            <img src={logo} alt="Algorithm Logo" className="p-1 md:p-6" />
                             <div className="text-center text-sm sm:text-xl lg:text-2xl">
                                 {name}
                             </div>
