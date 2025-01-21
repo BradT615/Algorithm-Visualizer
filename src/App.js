389bc22f1012caba3b2796aca95fcba4663f4ebb
BradT615 <125227539+BradT615@users.noreply.github.com>
2025-01-21 01:24:40 -0500

    new ui

--- src/App.js
+++ src/App.js
@@ -2,30 +2,29 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import './index.css';
 import AlgorithmComponents from './components/AlgorithmMapping';
 
 function App() {
     useEffect(() => {
         document.title = "Algorithm Visualizer";
+        // Add theme class to html element
+        document.documentElement.classList.add('theme-custom');
     }, []);
 
     return (
         <Router>
-            <div className='grid grid-rows-[auto,1fr] bg-customDarkBlue min-h-screen text-customGray'>
-                <Navbar />
-                <div className="flex flex-col justify-center items-center h-full w-full">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        {Object.keys(AlgorithmComponents).map(algoName => (
-                            <Route 
-                                key={algoName} 
-                                path={`/${algoName}`} 
-                                element={React.createElement(AlgorithmComponents[algoName])} 
-                            />
-                        ))}
-                    </Routes>
-                </div>
+            <Navbar /> 
+            <div className="flex flex-col justify-center items-center h-full w-full bg-background text-foreground">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    {Object.keys(AlgorithmComponents).map(algoName => (
+                        <Route 
+                            key={algoName} 
+                            path={`/${algoName}`} 
+                            element={React.createElement(AlgorithmComponents[algoName])} 
+                        />
+                    ))}
+                </Routes>
             </div>
         </Router>
     );
